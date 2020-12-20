75531b7704e4405ecbf8715fdf52fedfd5d83e33
Landon Harris <lharri73@vols.utk.edu>
2020-12-19 20:09:03 -0500

    init

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## send '-f' to force the container to be rebuild
+set -e
+
+main(){
+    if [[ "$1" == "-f" || "$(docker images -q pycpptest:main 2> /dev/null)" == "" ]]; then
+        docker build -t pycpptest:main .;
+    fi
+
+    loc=`pwd`/src
+
+    docker run -it --name pycppTest --rm -v $loc:/opt/app pycppTest:main 
+
+}
+
+checkDocker(){
+    if [ -f /.dockerenv ]; then
+        echo "ERROR: Cannot start container from within another container..."
+        echo -e "\tExiting!"
+        exit 1
+    fi
+}
+
+checkDocker
+main $@
